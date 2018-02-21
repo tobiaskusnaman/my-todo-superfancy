83382a85745e2ebfe9e757781d5c4865f463be8b
tobiaskusnaman <tobiaskusnaman@gmail.com>
2018-02-21 19:15:50 +0700

    add todo

---
+++ server/models/todo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+mongoose.connect('mongodb://localhost/toDoDb');
+
+var toDoSchema = new Schema({
+  name : String,
+  dueDate : Date,
+  status : Boolean,
+  userId: { type: Schema.Types.ObjectId, ref: 'User' },
+  assignedBy : { type: Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('toDo', toDoSchema)
